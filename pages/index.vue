bdb05885ba2621c42c753f5bde681e297657d902
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-07-21 21:45:34 +0600

    start a new project with nuxt 3 and taiiwind daisy ui

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <button class="btn btn-warning">Hello</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.hello {
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+</style>
